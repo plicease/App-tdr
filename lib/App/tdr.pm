06669f89fde665a116cd895ef6870af2bd143321
Graham Ollis <plicease@cpan.org>
2022-09-21 04:09:44 -0600

    Initial structure

---
+++ lib/App/tdr.pm
@@ -0,0 +1,12 @@
+package App::tdr;
+
+use strict;
+use warnings;
+use 5.008004;
+
+# ABSTRACT: Legacy Test Driver
+# VERSION
+
+1;
+
+
---
+++ t/app_tdr.t
@@ -0,0 +1,8 @@
+use Test2::V0 -no_srand => 1;
+use App::tdr;
+
+ok 1, 'todo';
+
+done_testing;
+
+
